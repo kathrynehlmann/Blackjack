032346749a603a4c1a0b9037a72cd6332e312e9e
kathrynehlmann <kathryn@wagnermediaworks.com>
2016-11-20 18:07:14 -0500

    cardfiles created and uploaded

--- project.js
+++ project.js
@@ -1,20 +1,16 @@
 console.log('Welcome to Project One');
 
 
+      //thinking an if else statement with functions in each part of the statement to compare the players hand to the dealers hand
+var playerHand = [];
 
-var playerHand = {
-  name: "Player"
-      //thinking an if else statement with functions in each part of the statement to compare the hand to the dealers hand
-}
+var dealerHand = [];
 
-  //thinking an if else statement with functions in each part to add or subtract from the players bank
-
-
-var dealerHand = {
-  name: "Dealer"
-    //thinking an if else statement with functions in each part of the statement to compare the hand to the players hand
-}
+var players = [playerHand, dealerHand];
 
 //then create a loop or nested loop to distribute cards to player and dealer
 
 //if else statement to check aces for values in each direction, using 11 as the indicator - "if less than 11 deal aceSpadehigh" "if more than 11 deal aceSpadelow"
+
+
+  //thinking an if else statement with functions in each part to add or subtract from the players bank
