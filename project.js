30e1c0879d41282c3d56343d5f72c1d7b1d35b2d
kathrynehlmann <kathryn@wagnermediaworks.com>
2016-11-22 17:12:30 -0500

    CLeaned up my code

--- project.js
+++ project.js
@@ -1,9 +1,5 @@
 console.log('Welcome to Project One');
 
-//attempting at first to simply create the cards in the console to get the game functional, then adding additional files to utilize graphics with the DOM, per Karolyn's recommendation
-
-//every card has the same image, the Ace of Spades, need to add these later
-
 var Card = function (suit, value, royal, image) {
   this.suit = suit;
   this.value = value;
@@ -13,29 +9,19 @@ var Card = function (suit, value, royal, image) {
 }
 var cardObjects = [];
 
-var cardFace = '23456789TJQK';
-var suit = '♤♡♢♧';
-var cards = [];
-//use a for loop to check the legnth of the suit (s for suit) and cardFace (f for cardFace)
+var cardFace = 'A23456789TJQK';
+var suit = ['spade', 'hearts', 'diamonds', 'clubs'];
+
 var makeCards = function() {
   var setSuit;
   var setRoyal;
   var setValue;
-  var setImage = "card_images/cardreverse.png"; // set to back of the card image
+  var setImage = "card_images/cardreverse.png"; // set image to back of the card.
 
   for (var s = 0; s < suit.length; s++) {
     for (var f = 1; f <= cardFace.length; f++) {
-      //if s = 0 then Spades
-      if (s === 0){setSuit = 'spade'}
-      //if s = 1 then Hearts
-      else if (s === 1){setSuit = 'hearts'}
-      //if s = 2 then Diamonds
-      else if (s === 2){setSuit = 'diamonds'}
-      //if s = 3 then Clubs
-      else {setSuit = 'clubs'}
 
-
-      // if f = 1 then value = 1 royal = ace
+      setSuit = suit[s];
       if (f === 1){setRoyal = 'ace'; setValue = 1}
       //if f = 10 then value = 10 royal = jack
       else if (f === 11){setRoyal = 'jack'; setValue = 10}
@@ -46,22 +32,16 @@ var makeCards = function() {
       //if f is in between 2 and 10 in the index number then the royal is going to be none
       else {setValue = f; setRoyal = "none"};
       setImage = "card_images/" + setSuit + setValue + setRoyal + ".png"
-      
+
       var card = new Card (setSuit, setValue, setRoyal, setImage);
       cardObjects.push(card);
 
-      cards.push(cardFace[f] + suit[s]);
-      // console.log(cards);
-      }
-    }
-  // console.log("I'm making cards", cardFace.length, suit.length);
-}
 
-//define an image key set of objects with set value within a for loop to add images with if else statement then append this image to the table
-//deck object to reset
+      } //end inner loop
+    } //end outer loop
+    console.log(cardObjects); //looking for missing cards
+}  //end of make card function
 
-// generate a random number to call on an index
-// look into disabling click events
 
 
 
@@ -74,35 +54,7 @@ function shuffle(o){ //v1.0
 }
 makeCards();
 
-////////////////////////////////////////////This works!
-  // var cardFace = '23456789TJQK';
-  // var suit = '♤♡♢♧';
-  // var cards = [];
-  // //use a for loop to check the legnth of the suit (s for suit) and cardFace (f for cardFace)
-  // var makeCards = function() {
-  //   for (var s = 0; s < suit.length; s++) {
-  //     for (var f = 0; f < cardFace.length; f++) {
-  //       cards.push(cardFace[f] + suit[s]);
-  //       console.log(cards);
-  //       }
-  //     }
-  //   console.log("I'm making cards", cardFace.length, suit.length);
-  // }
-  // //shuffle cards with math.random
-  // // credit to + Jonas Raoni Soares Silva
-  // //@ http://jsfromhell.com/array/shuffle [v1.0]
-  // function shuffle(o){ //v1.0
-  //     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  //     return o;
-  // }
-  // makeCards();
-  // //Cards are shuffled
-  // var displayCards = shuffle(cards);
 
-
-  //Step three then create a loop or nested loop to deal cards to player and dealer
-
-/////////////////////////////////////////////Jquery to show each card
   $(function() {
     var dealCard = function () {
       var shuffleCardobjects = shuffle(cardObjects);
@@ -175,8 +127,67 @@ $(function() {
 
 
 
+////Vectorized Playing Cards 2.0 - http://sourceforge.net/projects/vector-cards/
+// Copyright 2015 - Chris Aguilar - conjurenation@gmail.com
+// Licensed under LGPL 3 - www.gnu.org/copyleft/lesser.html
+
+// console.log("I'm making cards", cardFace.length, suit.length);
+// var cards = [];
+//use a for loop to check the legnth of the suit (s for suit) and cardFace (f for cardFace)
+//////////////////////////////////////////
+//Work on Understanding usiing the elements within the array versus simply usiing the legnth of the array within the loop. Simiplify the  if/else statements to access the array.
+// if f = 1 then value = 1 royal = ace
+//////////////////////////////////////////
+// cards.push(cardFace[f] + suit[s]);
+// console.log(cards);
+//////////////////////////////////////////
+//define an image key set of objects with set value within a for loop to add images with if else statement then append this image to the table
+//deck object to reset
+
+// generate a random number to call on an index
+// look into disabling click events
+/////////////////
+//attempting at first to simply create the cards in the console to get the game functional, then adding additional files to utilize graphics with the DOM, per Karolyn's recommendation
+
+//every card has the same image, the Ace of Spades, need to add these later
+////////////////////////////////////////////
+//if s = 0 then Spades
+// if (s === 0){setSuit = 'spade'}
+// //if s = 1 then Hearts
+// else if (s === 1){setSuit = 'hearts'}
+// //if s = 2 then Diamonds
+// else if (s === 2){setSuit = 'diamonds'}
+// //if s = 3 then Clubs
+// else {setSuit = 'clubs'}
+////////////////////////////////////////////This works!
+  // var cardFace = '23456789TJQK';
+  // var suit = '♤♡♢♧';
+  // var cards = [];
+  // //use a for loop to check the legnth of the suit (s for suit) and cardFace (f for cardFace)
+  // var makeCards = function() {
+  //   for (var s = 0; s < suit.length; s++) {
+  //     for (var f = 0; f < cardFace.length; f++) {
+  //       cards.push(cardFace[f] + suit[s]);
+  //       console.log(cards);
+  //       }
+  //     }
+  //   console.log("I'm making cards", cardFace.length, suit.length);
+  // }
+  // //shuffle cards with math.random
+  // // credit to + Jonas Raoni Soares Silva
+  // //@ http://jsfromhell.com/array/shuffle [v1.0]
+  // function shuffle(o){ //v1.0
+  //     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  //     return o;
+  // }
+  // makeCards();
+  // //Cards are shuffled
+  // var displayCards = shuffle(cards);
 
 
+  //Step three then create a loop or nested loop to deal cards to player and dealer
+
+/////////////////////////////////////////////Jquery to show each card
 
 /////////////////////////////////////////////////////////////////////////////
 //Cards are shuffled
